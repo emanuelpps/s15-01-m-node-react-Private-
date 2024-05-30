51fc8788befc83bd737903007e0a23b388279ffc
Adrian <esteban.adrian.gonz@gmail.com>
2024-05-30 18:33:14 -0300

    feat: set PRIVATE user routes

--- api/src/routers/UserRouter.js
+++ api/src/routers/UserRouter.js
@@ -1,6 +1,5 @@
 import MyRouter from "./MyRouter.js";
 import UserController from '../controllers/UserController.js'
-//import { verifyJWT } from '../middlewares/verifyJWT.js';
 
 export default class UserRouter extends MyRouter {
     constructor() {
@@ -9,11 +8,11 @@ export default class UserRouter extends MyRouter {
     }
 
     init() {
-        this.get('/', ["PUBLIC"], async(req, res) => this.userController.getAll(req, res))
-        this.get('/:id', ["PUBLIC"], async(req, res) => this.userController.getById(req, res))
-        this.post('/', ["PUBLIC"], async(req, res) => this.userController.create(req, res))
-        this.put('/:id', ["PUBLIC"], async(req, res) => this.userController.update(req, res))
-        this.delete('/:id', ["PUBLIC"], async(req, res) => this.userController.delete(req, res))
+        this.get('/', ["PRIVATE"], async(req, res) => this.userController.getAll(req, res))
+        this.get('/:id', ["PRIVATE"], async(req, res) => this.userController.getById(req, res))
+        this.post('/', ["PRIVATE"], async(req, res) => this.userController.create(req, res))
+        this.put('/:id', ["PRIVATE"], async(req, res) => this.userController.update(req, res))
+        this.delete('/:id', ["PRIVATE"], async(req, res) => this.userController.delete(req, res))
 
         this.post('/register', ["PUBLIC"], async(req, res) =>await this.userController.register(req, res));
         this.post('/login', ["PUBLIC"], async(req, res) => await this.userController.login(req, res));
