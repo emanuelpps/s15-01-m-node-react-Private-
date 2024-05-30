dd71e3afdfc95eb843a22578b41636d38586c83d
emanuelpps <emanuelpages.ps@gmail.com>
2024-05-30 09:35:03 -0300

    added register page and styles

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,7 +3,7 @@ import FormLoginContainer from "./formLogin/FormLoginContainer";
 function Login() {
   return (
     <div className="overflow-hidden bg-[#2C3E50]">
-      <div className="w-full h-screen md:w-screen md:h-screen flex justify-center items-center  ">
+      <div className="w-full h-screen md:w-screen md:h-screen flex justify-center items-center">
         <FormLoginContainer />
       </div>
     </div>
--- client/src/pages/login/register/RegisterFormContainer.jsx
+++ client/src/pages/login/register/RegisterFormContainer.jsx
@@ -1,15 +1,15 @@
-import RegisterButtons from "./RegisterButtons";
 import RegisterForm from "./RegisterForm";
 
 function RegisterFormContainer() {
   return (
     <div
       id="form-login-container"
-      className="flex flex-col justify-center items-center w-[50%] gap-10 bg-[#C2BEBE] p-10"
+      className="flex flex-col justify-center items-center w-[300px] md:w-[40%] md:max-h-[95%] gap-10 background-conic p-10 rounded-xl shadow-lg"
     >
-      <h1 className="text-3xl">Crear una cuenta</h1>
+      <h1 className="text-3xl text-shadow-title text-[#ffff]">
+        Crear una cuenta
+      </h1>
       <RegisterForm />
-      <RegisterButtons />
     </div>
   );
 }
