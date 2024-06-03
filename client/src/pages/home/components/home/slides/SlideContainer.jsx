e96255e35fda366f41f7399358c8e93befe3af0f
emanuelpps <emanuelpages.ps@gmail.com>
2024-06-03 13:45:51 -0300

    added button in homepage slider

--- client/src/pages/home/components/home/slides/SlideContainer.jsx
+++ client/src/pages/home/components/home/slides/SlideContainer.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/no-unknown-property */
 import { useState } from "react";
 import sliderImg from "../../../../../assets/images/slideImg.png";
 import rectangle from "../../../../../assets/icons/Rectangle.svg";
 import ellipse from "../../../../../assets/icons/Ellipse.svg";
+import Buttons from "../../../../../components/Buttons";
 function SlideContainer() {
   const [currentSlide, setCurrentSlide] = useState(0);
   //const [imgSelector, setImgSelector] = useState(0);
@@ -27,6 +29,15 @@ function SlideContainer() {
                   currentSlide === index ? "block" : "hidden"
                 }`}
               />
+              <div
+                className={`${
+                  currentSlide === index
+                    ? "block absolute left-[30%] pt-[10%]"
+                    : "hidden"
+                }`}
+              >
+                <Buttons variant="primary">Ver Trailer</Buttons>
+              </div>
             </div>
           ))}
         </div>
