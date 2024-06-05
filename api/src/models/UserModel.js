694636de3c158d26ee1450424b0ba5b80959f257
Adrian <esteban.adrian.gonz@gmail.com>
2024-06-05 02:53:50 -0300

    chore: add active field to user model

--- api/src/models/UserModel.js
+++ api/src/models/UserModel.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema({
       type: String,
       required: true,
     },
+    active: {
+      type: Boolean,
+      default: true
+    },
     created_at: {
       type: Date,
       default: Date.now,
