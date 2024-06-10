3afb3b22eb9516ed933f56e23ad67f2c497484ae
emanuelpps <emanuelpages.ps@gmail.com>
2024-06-09 21:05:57 -0300

    added full structure

--- client/src/pages/uploadVideo/UploadVideo.jsx
+++ client/src/pages/uploadVideo/UploadVideo.jsx
@@ -1,9 +1,13 @@
-
+import Table from "./Table/Table";
+import Upload from "./Upload/Upload";
 
 function UploadVideo() {
   return (
-    <div>UploadVideo</div>
-  )
+    <div className="h-screen flex flex-col w-full bg-[#2C3E50] items-center overflow-x-hidden">
+      <Table />
+      <Upload />
+    </div>
+  );
 }
 
-export default UploadVideo
+export default UploadVideo;
