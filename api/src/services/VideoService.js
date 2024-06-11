0b2c6a133bbf3edc53ea28827d174e2b568cf776
Adrian <esteban.adrian.gonz@gmail.com>
2024-06-11 11:52:14 -0300

    ref: adjust create videoModel to new fields

--- api/src/services/VideoService.js
+++ api/src/services/VideoService.js
@@ -8,36 +8,39 @@ export default class VideoService{
     update = async (id, data) => await videoModel.updateOne({ _id: id }, data);
     delete = async (id) => await videoModel.deleteOne({ _id: id });
 
-    async postVideo(video, videoData){
+    async postVideo({ video }, videoData){
         let result_video;
         let videom = new videoModel();
         console.log('service postVideo, video, videoData: ', video, videoData)
         if (!video) {
             return { msg: 'No File Selected. Please select an file and try' };
         }
-        if(video){
-        console.log("video to be save", video.tempFilePath)}
         result_video = await cloudinary.uploader.upload(video.tempFilePath, 
           { resource_type: "video", 
-            public_id: "video_prueba",
+            public_id: videoData.title,
             eager: [
               { width: 300, height: 300, crop: "pad", audio_codec: "none" }, 
               { width: 160, height: 100, crop: "crop", gravity: "south", audio_codec: "none" } ],                                   
             eager_async: true})
-            //eager_notification_url: "https://mysite.example.com/notify_endpoint" })
-        // Create Image in the database with all data that we have and add to it the
-        // image information that we get from Cloudinary
-        console.log("video info to be save")
 
-        //image.fechaCreacion =new Date().getTime;
         videom = await videoModel.create({
             idCloudinary : result_video.public_id,
             url : result_video.secure_url,
             title: videoData.title,
             description: videoData.description,
-            created_at: Date.now()
+            created_at: Date.now(),
+            cast: videoData.cast,
+            genre: videoData.genre.toLowerCase(),
+            rating: videoData.rating,
+            releaseDate: videoData.releaseDate,
+            videoLength: {
+                hours: videoData.hours,
+                minutes: videoData.minutes,
+                seconds: videoData.seconds
+            },
+            videoType: videoData.videoType.toLowerCase(),
+            views: videoData.views,
         })
         return video
     }
-
 }
