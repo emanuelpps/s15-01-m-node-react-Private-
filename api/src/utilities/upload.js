9fab9945633a5383fb2554e5b1d0473502c1026b
Adrian <esteban.adrian.gonz@gmail.com>
2024-06-12 01:35:32 -0300

    hotfix: revert keys at cloudinary config file

--- api/src/utilities/upload.js
+++ api/src/utilities/upload.js
@@ -4,12 +4,9 @@ import dotenv  from "dotenv";
 dotenv.config()
 
 cloudinary.config({
-    // cloud_name: process.env.CLOUD_NAME,
-    cloud_name: do9aerh2z,
-    // api_key: process.env.API_KEY,
-    api_key: 683629718316347,
-    // api_secret: process.env.API_SECRET,
-    api_secret: 'sgXOZ9m4fF6mdkOJWgqGtWsMYJk',
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
 })
 
 export default cloudinary;
