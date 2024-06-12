9776f4e489ac8f6aeb849fb4212e379fcca0c830
Adrian <esteban.adrian.gonz@gmail.com>
2024-06-12 01:25:07 -0300

    hotfix: change keys at cloudinary config file

--- api/src/utilities/upload.js
+++ api/src/utilities/upload.js
@@ -4,9 +4,12 @@ import dotenv  from "dotenv";
 dotenv.config()
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
+    // cloud_name: process.env.CLOUD_NAME,
+    cloud_name: do9aerh2z,
+    // api_key: process.env.API_KEY,
+    api_key: 683629718316347,
+    // api_secret: process.env.API_SECRET,
+    api_secret: 'sgXOZ9m4fF6mdkOJWgqGtWsMYJk',
 })
 
 export default cloudinary;
